fe5d82929cfaacc467bab378b7588a513c5b23ec
SimonCropp <simon.cropp@gmail.com>
2020-04-16 21:32:16 +1000

    failing test for multimatch dir

--- src/DiffEngine.Tests/WildcardFileFinderTests.cs
+++ src/DiffEngine.Tests/WildcardFileFinderTests.cs
@@ -1,10 +1,35 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Xunit;
 using Xunit.Abstractions;
 
 public class WildcardFileFinderTests :
     XunitContextBase
 {
+    [Fact]
+    public void MultiMatchDir_order1()
+    {
+        var dir1 = Path.Combine(SourceDirectory, @"DirForSearch\dir1");
+        var dir2 = Path.Combine(SourceDirectory, @"DirForSearch\dir2");
+        Directory.SetLastWriteTime(dir2, DateTime.Now.AddDays(-1));
+        Directory.SetLastWriteTime(dir1, DateTime.Now);
+        var path = Path.Combine(SourceDirectory, @"DirForSearch\*\TextFile1.txt");
+        Assert.True(WildcardFileFinder.TryFind(path, out var result));
+        Assert.True(File.Exists(result));
+    }
+
+    [Fact]
+    public void MultiMatchDir_order2()
+    {
+        var dir1 = Path.Combine(SourceDirectory, @"DirForSearch\dir1");
+        var dir2 = Path.Combine(SourceDirectory, @"DirForSearch\dir2");
+        Directory.SetLastWriteTime(dir1, DateTime.Now.AddDays(-1));
+        Directory.SetLastWriteTime(dir2, DateTime.Now);
+        var path = Path.Combine(SourceDirectory, @"DirForSearch\*\TextFile1.txt");
+        Assert.True(WildcardFileFinder.TryFind(path, out var result));
+        Assert.True(File.Exists(result));
+    }
+
     [Fact]
     public void FullFilePath()
     {
