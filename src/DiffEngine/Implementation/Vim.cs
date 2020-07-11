e79b32a957bbe02c6259fd1b7cac24fb91e4463e
Simon Cropp <simon.cropp@gmail.com>
2020-07-11 16:45:31 +1000

    Create Vim.cs

---
+++ src/DiffEngine/Implementation/Vim.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DiffEngine;
+
+static partial class Implementation
+{
+    public static Definition Vim()
+    {
+        string Arguments(string temp, string target)
+        {
+            return $"-d \"{temp}\" \"{target}\" -c \"setl autoread | setl nobackup\"";
+        }
+
+        return new Definition(
+            name: DiffTool.Vim,
+            url: "https://www.vim.org/",
+            autoRefresh: true,
+            isMdi: false,
+            supportsText: true,
+            requiresTarget: true,
+            binaryExtensions: Array.Empty<string>(),
+            windows: new OsSettings(Arguments, @"%ProgramFiles%\Vim\*\vim.exe"),
+            notes: @"
+ * [Options](http://vimdoc.sourceforge.net/htmldoc/options.html)
+ * `autoread`: When a file has been detected to have been changed outside of Vim and
+	it has not been changed inside of Vim, automatically read it again");
+    }
+}
