7f944423321e5b38a9dd46d4bf8e59afd3f9e55c
Simon Cropp <simon.cropp@gmail.com>
2020-08-20 10:33:59 +1000

    default shellExecute = true

--- src/DiffEngine/Definition.cs
+++ src/DiffEngine/Definition.cs
@@ -29,7 +29,7 @@ class Definition
         OsSettings? linux = null,
         OsSettings? osx = null,
         string? notes = null,
-        bool shellExecute = false)
+        bool shellExecute = true)
     {
         Tool = name;
         Url = url;
