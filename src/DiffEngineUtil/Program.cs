aaf09ce0c0bffcec8d2b04b2210498707fd33008
Simon Cropp <simon.cropp@gmail.com>
2020-08-22 20:11:13 +1000

    .

--- src/DiffEngineUtil/Program.cs
+++ src/DiffEngineUtil/Program.cs
@@ -10,19 +10,17 @@ static class Program
     {
         using var iconStream = Resource.AsStream("icon.ico");
         using var icon = new Icon(iconStream);
-        var notifyIcon = new NotifyIcon
+        using (new NotifyIcon
         {
             Icon = icon,
             Visible = true,
             BalloonTipText = "Hello from My Kitten",
             BalloonTipTitle = "Cat Talk",
-            BalloonTipIcon = ToolTipIcon.Info
-        };
-
-        notifyIcon.ContextMenuStrip = new ContextMenuStrip();
-        //icon.ShowBalloonTip(2000);
-
-        //Application.Run(new Form1());
-        new ManualResetEvent(false).WaitOne();
+            BalloonTipIcon = ToolTipIcon.Info,
+            ContextMenuStrip = new ContextMenuStrip()
+        })
+        {
+            new ManualResetEvent(false).WaitOne();
+        }
     }
 }
