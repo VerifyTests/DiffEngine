87c86aac4428ab947e33943991aa07d7367133dd
Simon Cropp <simon.cropp@gmail.com>
2020-08-22 21:06:21 +1000

    Update Program.cs

--- src/DiffEngineUtil/Program.cs
+++ src/DiffEngineUtil/Program.cs
@@ -6,21 +6,36 @@ using Resourcer;
 
 static class Program
 {
+    static Mutex mutex;
+
     static void Main()
     {
+        mutex = new Mutex(true, "DiffEngineUtil", out var createdNew);
+        if (!createdNew)
+        {
+            mutex.Dispose();
+            Environment.Exit(0);
+        }
+
+        //if()
         using var iconStream = Resource.AsStream("icon.ico");
         using var icon = new Icon(iconStream);
-        using (new NotifyIcon
+        var contextMenuStrip = new ContextMenuStrip();
+        contextMenuStrip.Items.Add(new ToolStripButton("Exit"));
+        using (var notifyIcon = new NotifyIcon
         {
             Icon = icon,
             Visible = true,
             BalloonTipText = "Hello from My Kitten",
             BalloonTipTitle = "Cat Talk",
             BalloonTipIcon = ToolTipIcon.Info,
-            ContextMenuStrip = new ContextMenuStrip()
+            ContextMenuStrip = contextMenuStrip,
         })
         {
+            Application.Run();
             new ManualResetEvent(false).WaitOne();
+
+            mutex.Dispose();
         }
     }
 }
