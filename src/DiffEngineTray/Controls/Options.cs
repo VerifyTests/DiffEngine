c6c9a5979dc89fe78aad0414bf6fba95fe83c4df
Simon Cropp <simon.cropp@gmail.com>
2020-08-31 19:12:22 +1000

    .

---
+++ src/DiffEngineTray/Controls/Options.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiffEngineTray.Controls
+{
+    public partial class Options : Form
+    {
+        public Options()
+        {
+            InitializeComponent();
+        }
+    }
+}
