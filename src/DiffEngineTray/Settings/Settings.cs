f3093246f0390de648789b916199814a82fce10c
Simon Cropp <simon.cropp@gmail.com>
2020-08-31 21:08:33 +1000

    .

---
+++ src/DiffEngineTray/Settings/Settings.cs
@@ -0,0 +1,4 @@
+public class Settings
+{
+    public string? AcceptHotKeys { get; set; }
+}
---
+++ src/DiffEngineTray/Settings/SettingsHelper.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class SettingsHelper
+{
+    public static string FilePath;
+
+    static SettingsHelper()
+    {
+        var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        FilePath = Path.Combine(appData, "DiffEngine", "settings.json");
+    }
+
+    public static async Task<Settings> Read()
+    {
+        if (File.Exists(FilePath))
+        {
+            await using var stream = File.OpenRead(FilePath);
+            return await JsonSerializer.DeserializeAsync<Settings>(stream);
+        }
+
+        await File.WriteAllTextAsync(FilePath, "{}");
+        return new Settings();
+    }
+
+    public static async Task Write(Settings settings)
+    {
+        await using var stream = File.OpenWrite(FilePath);
+        await JsonSerializer.SerializeAsync(stream, settings);
+    }
+}
