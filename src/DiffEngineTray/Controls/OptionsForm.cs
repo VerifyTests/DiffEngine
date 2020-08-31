cbd2f988203476e26ca6d2fb02476bcdcc00df36
Simon Cropp <simon.cropp@gmail.com>
2020-08-31 19:44:17 +1000

    .

--- src/DiffEngineTray/Controls/OptionsForm.cs
+++ src/DiffEngineTray/Controls/OptionsForm.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-    public partial class OptionsForm : Form
+public partial class OptionsForm : Form
+{
+    public OptionsForm()
     {
-        public OptionsForm()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
+}
