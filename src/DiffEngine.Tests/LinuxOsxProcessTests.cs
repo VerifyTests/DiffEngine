4ec073b848f8b25302f217a0d69c87bb0f75df62
Simon Cropp <simon.cropp@gmail.com>
2020-09-02 19:37:39 +1000

    fix linux date parsing

--- src/DiffEngine.Tests/LinuxOsxProcessTests.cs
+++ src/DiffEngine.Tests/LinuxOsxProcessTests.cs
@@ -15,6 +15,7 @@ public class LinuxOsxProcessTests :
         Assert.Equal(new DateTime(2020,8,26,2,17,40), processCommand.StartTime);
         Assert.Equal("/System/Library/coreauthd -foo", processCommand.Command);
     }
+
     [Fact]
     public void TryParse_noSlash()
     {
@@ -23,6 +24,17 @@ public class LinuxOsxProcessTests :
         var processCommand = command!.Value;
         Assert.Equal(309, processCommand.Process);
         Assert.Equal(new DateTime(2020,8,26,2,17,40), processCommand.StartTime);
+        Assert.Equal("System/Library/coreauthd -foo", processCommand.Command);
+    }
+
+    [Fact]
+    public void TryParse_singleDigit()
+    {
+        var parse = LinuxOsxProcess.TryParse("309 Thu Aug 6 02:17:40 2020     System/Library/coreauthd -foo", out var command);
+        Assert.True(parse);
+        var processCommand = command!.Value;
+        Assert.Equal(309, processCommand.Process);
+        Assert.Equal(new DateTime(2020,8,6,2,17,40), processCommand.StartTime);
         Assert.Equal("System/Library/coreauthd -foo", processCommand.Command);
     }
 
