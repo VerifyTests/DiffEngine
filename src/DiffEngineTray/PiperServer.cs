820ed51685f90f97654b5c3c60b8adb76ac8cf6a
Simon Cropp <simon.cropp@gmail.com>
2020-09-04 21:34:31 +1000

    Update PiperServer.cs

--- src/DiffEngineTray/PiperServer.cs
+++ src/DiffEngineTray/PiperServer.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 static class PiperServer
 {
     public static async Task Start(
-        Action<MovePayload> receiveMove,
-        Action<DeletePayload> receiveDelete,
+        Action<MovePayload> move,
+        Action<DeletePayload> delete,
         CancellationToken cancellation = default)
     {
         while (true)
@@ -20,7 +20,7 @@ static class PiperServer
 
             try
             {
-                await Handle(receiveMove, receiveDelete, cancellation);
+                await Handle(move, delete, cancellation);
             }
             catch (TaskCanceledException)
             {
@@ -33,7 +33,7 @@ static class PiperServer
         }
     }
 
-    static async Task Handle(Action<MovePayload> receiveMove, Action<DeletePayload> receiveDelete, CancellationToken cancellation)
+    static async Task Handle(Action<MovePayload> move, Action<DeletePayload> delete, CancellationToken cancellation)
     {
         await using var pipe = new NamedPipeServerStream(
             "DiffEngine",
@@ -48,12 +48,12 @@ static class PiperServer
         if (payload.Contains("\"Type\":\"Move\""))
         {
             var movePayload = Serializer.Deserialize<MovePayload>(payload);
-            receiveMove(movePayload);
+            move(movePayload);
         }
         else if (payload.Contains("\"Type\":\"Delete\""))
         {
             var deletePayload = Serializer.Deserialize<DeletePayload>(payload);
-            receiveDelete(deletePayload);
+            delete(deletePayload);
         }
         else
         {
