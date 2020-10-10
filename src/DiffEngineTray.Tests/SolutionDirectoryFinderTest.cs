f2c9a46ee39a90e561b891b89419a67709dbb053
Simon Cropp <simon.cropp@gmail.com>
2020-10-11 09:20:42 +1100

    SolutionDirectoryFinder

---
+++ src/DiffEngineTray.Tests/SolutionDirectoryFinderTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+[UsesVerify]
+public class SolutionDirectoryFinderTests :
+    XunitContextBase
+{
+    public SolutionDirectoryFinderTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+
+    [Fact]
+    public async Task TryFind()
+    {
+        var found = SolutionDirectoryFinder.TryFind(SourceFile, out var path);
+        await Verifier.Verify(new {found, path});
+    }
+}
---
+++ src/DiffEngineTray/SolutionDirectoryFinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+static class SolutionDirectoryFinder
+{
+    static ConcurrentDictionary<string, string?> cache = new ConcurrentDictionary<string, string?>(StringComparer.OrdinalIgnoreCase);
+
+    public static bool TryFind(string file, [NotNullWhen(true)] out string? path)
+    {
+        path = cache.GetOrAdd(file, Inner);
+        return path != null;
+    }
+
+    static string? Inner(string file)
+    {
+        var currentDirectory = Path.GetDirectoryName(file)!;
+        do
+        {
+            var solutions = Directory.GetFiles(currentDirectory, "*.sln");
+            if (solutions.Any())
+            {
+                return Path.GetFileNameWithoutExtension(solutions.First());
+            }
+
+            var parent = Directory.GetParent(currentDirectory);
+            if (parent == null)
+            {
+                return null;
+            }
+
+            currentDirectory = parent.FullName;
+        } while (true);
+    }
+}
