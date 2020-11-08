d3530b54b50784ac7a00880af4da13f48d1701cf
Simon Cropp <simon.cropp@gmail.com>
2020-11-08 23:08:29 +1100

    re verify

--- src/DiffEngineTray.Tests/ModuleInitializer.cs
+++ src/DiffEngineTray.Tests/ModuleInitializer.cs
@@ -4,7 +4,6 @@ public static class ModuleInitializer
 {
     public static void Initialize()
     {
-        VerifierSettings.DisableNewLineEscaping();
         VerifyWinForms.Enable();
         VerifyImageMagick.Initialize();
         VerifyImageMagick.RegisterComparers();
