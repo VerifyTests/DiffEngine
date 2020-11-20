7f857ca0e1ccbdd4a3ed2aaa057bbac7d8a2cda6
Simon Cropp <simon.cropp@gmail.com>
2020-11-20 18:32:46 +1100

    suppress noise

--- src/DiffEngine.Tests/AssemblyLocation.cs
+++ src/DiffEngine.Tests/AssemblyLocation.cs
@@ -1,11 +1,17 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 static class AssemblyLocation
 {
     static AssemblyLocation()
     {
         var assembly = typeof(AssemblyLocation).Assembly;
-        CurrentDirectory = Path.GetDirectoryName(assembly.Location)!;
+#pragma warning disable 618
+        var uri = new UriBuilder(assembly.CodeBase!);
+#pragma warning restore 618
+        var path = Uri.UnescapeDataString(uri.Path);
+
+        CurrentDirectory = Path.GetDirectoryName(path)!;
     }
 
     public static string CurrentDirectory;
