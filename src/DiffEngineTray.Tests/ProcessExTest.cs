24e0aefbbb2049cf7dd305f07b7be7f7b67cc12f
Simon Cropp <simon.cropp@gmail.com>
2021-07-21 10:29:22 +1000

    Create ProcessExTest.cs

---
+++ src/DiffEngineTray.Tests/ProcessExTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+public class ProcessExTest
+{
+    [Fact]
+    public void AddSingle()
+    {
+        using var current = Process.GetCurrentProcess();
+        Assert.True(ProcessEx.TryGet(current.Id, out var found));
+        Assert.NotNull(found);
+        found!.Dispose();
+    }
+}
