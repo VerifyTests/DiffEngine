fea8c6a636b6c7d78b5409b54853971868fd3734
Simon Cropp <simon.cropp@gmail.com>
2021-07-26 08:58:35 +1000

    Update SettingsValidator.cs

--- src/DiffEngineTray/Settings/SettingsValidator.cs
+++ src/DiffEngineTray/Settings/SettingsValidator.cs
@@ -7,6 +7,7 @@ public static class SettingsValidator
     {
         errors = new();
         ValidateHotKey(errors, settings.AcceptAllHotKey);
+        ValidateHotKey(errors, settings.AcceptOpenHotKey);
 
         return !errors.Any();
     }
