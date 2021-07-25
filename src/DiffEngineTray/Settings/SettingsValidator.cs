35226dec7361634f01c1a4df7a1ef23fb2692f8e
Simon Cropp <simon.cropp@gmail.com>
2021-07-26 08:58:09 +1000

    Update SettingsValidator.cs

--- src/DiffEngineTray/Settings/SettingsValidator.cs
+++ src/DiffEngineTray/Settings/SettingsValidator.cs
@@ -6,20 +6,26 @@ public static class SettingsValidator
     public static bool IsValidate(this Settings settings, out List<string> errors)
     {
         errors = new();
-        var hotKey = settings.AcceptAllHotKey;
-        if (hotKey != null)
-        {
-            if (!hotKey.Alt && !hotKey.Shift && !hotKey.Control)
-            {
-                errors.Add("HotKey: At least one modifier is required");
-            }
-
-            if (string.IsNullOrWhiteSpace(hotKey.Key))
-            {
-                errors.Add("HotKey: key is required");
-            }
-        }
+        ValidateHotKey(errors, settings.AcceptAllHotKey);
 
         return !errors.Any();
+    }
+
+    static void ValidateHotKey(List<string> errors, HotKey? hotKey)
+    {
+        if (hotKey == null)
+        {
+            return;
+        }
+
+        if (!hotKey.Alt && !hotKey.Shift && !hotKey.Control)
+        {
+            errors.Add("HotKey: At least one modifier is required");
+        }
+
+        if (string.IsNullOrWhiteSpace(hotKey.Key))
+        {
+            errors.Add("HotKey: key is required");
+        }
     }
 }
