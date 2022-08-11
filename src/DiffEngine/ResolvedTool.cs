146d7d1b3546812971dc559ada23127ea956c68f
Simon Cropp <simon.cropp@gmail.com>
2022-08-12 09:35:10 +1000

    Update ResolvedTool.cs

--- src/DiffEngine/ResolvedTool.cs
+++ src/DiffEngine/ResolvedTool.cs
@@ -1,18 +1,16 @@
 ﻿namespace DiffEngine;
 
-[DebuggerDisplay("{Name} {ExePath}, Refresh={AutoRefresh}, Mdi={IsMdi}, RequiresTarget={RequiresTarget}, SupportsText={SupportsText}")]
-public class ResolvedTool
+public record ResolvedTool(
+    string Name,
+    DiffTool? Tool,
+    string ExePath,
+    LaunchArguments LaunchArguments,
+    bool IsMdi,
+    bool AutoRefresh,
+    IReadOnlyCollection<string> BinaryExtensions,
+    bool RequiresTarget,
+    bool SupportsText)
 {
-    public string Name { get; }
-    public DiffTool? Tool { get; }
-    public string ExePath { get; }
-    public LaunchArguments LaunchArguments { get; }
-    public bool IsMdi { get; }
-    public bool AutoRefresh { get; }
-    public IReadOnlyCollection<string> BinaryExtensions { get; }
-    public bool RequiresTarget { get; }
-    public bool SupportsText { get; }
-
     internal void CommandAndArguments(string tempFile, string targetFile, out string arguments, out string command)
     {
         arguments = GetArguments(tempFile, targetFile);
@@ -32,47 +30,10 @@ public class ResolvedTool
         return LaunchArguments.Right(tempFile, targetFile);
     }
 
-    internal ResolvedTool(
-        string name,
-        DiffTool? tool,
-        string exePath,
-        LaunchArguments launchArguments,
-        bool isMdi,
-        bool autoRefresh,
-        IReadOnlyCollection<string> binaryExtensions,
-        bool requiresTarget,
-        bool supportsText)
-    {
-        Name = name;
-        Tool = tool;
-        ExePath = exePath;
-        LaunchArguments = launchArguments;
-        IsMdi = isMdi;
-        AutoRefresh = autoRefresh;
-        BinaryExtensions = binaryExtensions;
-        RequiresTarget = requiresTarget;
-        SupportsText = supportsText;
-    }
-
-    public ResolvedTool(
-        string name,
-        string exePath,
-        LaunchArguments launchArguments,
-        bool isMdi,
-        bool autoRefresh,
-        IReadOnlyCollection<string> binaryExtensions,
-        bool requiresTarget,
-        bool supportsText)
+    public ResolvedTool(string name, string exePath, LaunchArguments launchArguments, bool isMdi, bool autoRefresh, IReadOnlyCollection<string> binaryExtensions, bool requiresTarget, bool supportsText) :
+        this(name, null, exePath, launchArguments, isMdi, autoRefresh, binaryExtensions, requiresTarget, supportsText)
     {
         Guard.FileExists(exePath, nameof(exePath));
         Guard.AgainstEmpty(name, nameof(name));
-        Name = name;
-        ExePath = exePath;
-        LaunchArguments = launchArguments;
-        IsMdi = isMdi;
-        AutoRefresh = autoRefresh;
-        BinaryExtensions = binaryExtensions;
-        RequiresTarget = requiresTarget;
-        SupportsText = supportsText;
     }
 }
