2f75f36af17180ad0d3a0f69ad1ec6e193b77039
Simon Cropp <simon.cropp@gmail.com>
2022-12-16 07:39:24 +1100

    Update FakeDiffTool.cs

--- src/DiffEngine.Tests/FakeDiffTool.cs
+++ src/DiffEngine.Tests/FakeDiffTool.cs
@@ -1,21 +1,29 @@
 ﻿public class FakeDiffTool
 {
-    public static string Exe;
+    public static string ExePath;
+    public static string ExeName = null!;
 
     static FakeDiffTool()
     {
+        var directory = string.Empty;
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            Exe = Path.GetFullPath(Path.Combine(AssemblyLocation.CurrentDirectory, "../../../../FakeDiffTool/bin/win-x64/FakeDiffTool.exe"));
-            return;
+            ExeName = "FakeDiffTool.exe";
+            directory = "../../../../FakeDiffTool/bin/win-x64/FakeDiffTool.exe";
         }
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
-            Exe = Path.GetFullPath(Path.Combine(AssemblyLocation.CurrentDirectory, "../../../../FakeDiffTool/bin/osx-x64/FakeDiffTool"));
-            return;
+            ExeName = "FakeDiffTool";
+            directory = "../../../../FakeDiffTool/bin/osx-x64";
         }
 
-        throw new();
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            ExeName = "FakeDiffTool";
+            directory = "../../../../FakeDiffTool/bin/linux-x64";
+        }
+
+        ExePath = Path.GetFullPath(Path.Combine(AssemblyLocation.CurrentDirectory, directory, ExeName));
     }
 }
