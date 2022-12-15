dd1653538eac00cecf47a3cdf22f5a27f9d38398
Simon Cropp <simon.cropp@gmail.com>
2022-12-16 08:12:16 +1100

    Update FakeDiffTool.cs

--- src/DiffEngine.Tests/FakeDiffTool.cs
+++ src/DiffEngine.Tests/FakeDiffTool.cs
@@ -1,18 +1,24 @@
 ﻿public class FakeDiffTool
 {
     public static string ExePath;
+    public static string ExeName;
+
     static FakeDiffTool()
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
+            ExeName = "FakeDiffTool.exe";
             ExePath = Path.GetFullPath(Path.Combine(AssemblyLocation.CurrentDirectory, "../../../../FakeDiffTool/bin/win-x64/FakeDiffTool.exe"));
             return;
         }
+
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
+            ExeName = "FakeDiffTool";
             ExePath = Path.GetFullPath(Path.Combine(AssemblyLocation.CurrentDirectory, "../../../../FakeDiffTool/bin/osx-x64/FakeDiffTool"));
             return;
         }
+
         throw new();
     }
 }
