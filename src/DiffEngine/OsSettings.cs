283ff18cd81911599de6d06fe747c2d8e224a8c2
Simon Cropp <simon.cropp@gmail.com>
2023-10-10 22:38:30 +1100

    support custom path command name

--- src/DiffEngine/OsSettings.cs
+++ src/DiffEngine/OsSettings.cs
@@ -1,12 +1,41 @@
-public record OsSettings(string ExeName, LaunchArguments LaunchArguments, params string[] SearchDirectories)
+public record OsSettings(string ExeName, string PathCommandName, LaunchArguments LaunchArguments, params string[] SearchDirectories)
 {
+    public OsSettings(string exeName, string pathCommandName, LaunchArguments launchArguments, string searchDirectory) :
+        this(
+            exeName,
+            pathCommandName,
+            launchArguments,
+            new[]
+            {
+                searchDirectory
+            })
+    {
+    }
+
+    public OsSettings(string exeName, LaunchArguments launchArguments, params string[] searchDirectories) :
+        this(exeName, exeName, launchArguments, searchDirectories)
+    {
+    }
+
     public OsSettings(string exeName, LaunchArguments launchArguments, string searchDirectory) :
-        this(exeName, launchArguments, new[] {searchDirectory})
+        this(
+            exeName,
+            exeName,
+            launchArguments,
+            new[]
+            {
+                searchDirectory
+            })
+    {
+    }
+
+    public OsSettings(string exeName, string pathCommandName, LaunchArguments launchArguments) :
+        this(exeName, pathCommandName, launchArguments, Array.Empty<string>())
     {
     }
 
     public OsSettings(string exeName, LaunchArguments launchArguments) :
-        this(exeName, launchArguments, Array.Empty<string>())
+        this(exeName, exeName, launchArguments, Array.Empty<string>())
     {
     }
 }
