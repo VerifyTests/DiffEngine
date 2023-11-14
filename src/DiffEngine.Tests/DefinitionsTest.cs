3bcfa3ca4de8fcc1133a45ca3cf1b901d0b6cc0c
Simon Cropp <simon.cropp@gmail.com>
2023-11-14 20:42:48 +1100

    Revert "Update DefinitionsTest.cs"
    This reverts commit f0c7e56b65f9f1acdbd1a399312227f260310a80.

--- src/DiffEngine.Tests/DefinitionsTest.cs
+++ src/DiffEngine.Tests/DefinitionsTest.cs
@@ -206,7 +206,7 @@ public class DefinitionsTest(ITestOutputHelper output) :
 
         foreach (var path in paths)
         {
-            writer.WriteLine($"    * `{Path.Combine(path, exeName)}`");
+            writer.WriteLine($"    * `{path}{exeName}`");
         }
 
         writer.WriteLine($"    * `%PATH%{pathCommandName}`");
