7911d5cea635cf1fbbd44cfbecfb11626fb03f69
Simon Cropp <simon.cropp@gmail.com>
2024-03-07 16:41:26 +1100

    Revert "Update DiffToolsTest.cs"
    This reverts commit 7d86e13e6630c102380eff13f5c2bac7c10b2149.

--- src/DiffEngine.Tests/DiffToolsTest.cs
+++ src/DiffEngine.Tests/DiffToolsTest.cs
@@ -59,9 +59,10 @@
         Assert.Equal("MyCustomDiffTool", forExtension.Name);
     }
 
-    static void AddToolBasedOn()
+#if DEBUG
+    [Fact]
+    public void AddToolBasedOn()
     {
-        // ReSharper disable once UnusedVariable
         #region AddToolBasedOn
 
         var resolvedTool = DiffTools.AddToolBasedOn(
@@ -73,7 +74,13 @@
 
         #endregion
 
+        Assert.Equal(resolvedTool, DiffTools.Resolved.First());
+        Assert.True(DiffTools.TryFindByExtension(".txt", out var forExtension));
+        Assert.Equal(resolvedTool, forExtension);
+        Assert.Equal("\"custom args \"bar\" \"foo\"", resolvedTool.LaunchArguments.Left("foo", "bar"));
+        Assert.Equal("\"custom args \"foo\" \"bar\"", resolvedTool.LaunchArguments.Right("foo", "bar"));
     }
+#endif
     static async Task AddToolAndLaunch()
     {
         #region AddToolAndLaunch
