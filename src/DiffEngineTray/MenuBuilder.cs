d8ea8d3b321803c81b57f6d54e72022369f22932
Simon Cropp <simon.cropp@gmail.com>
2024-07-18 17:24:31 +1000

    add file purging

--- src/DiffEngineTray/MenuBuilder.cs
+++ src/DiffEngineTray/MenuBuilder.cs
@@ -23,28 +23,25 @@ static class MenuBuilder
         items.Add(new MenuButton("Exit", exit, Images.Exit));
         items.Add(new MenuButton("Options", launchOptions, Images.Options));
         items.Add(new MenuButton("Open logs", Logging.OpenDirectory, Images.Folder));
+        items.Add(new MenuButton("Purge verified files", FilePurger.Launch, Images.Folder));
         items.Add(new MenuButton("Raise issue", IssueLauncher.Launch, Images.Link));
         return menu;
     }
 
-    static IEnumerable<ToolStripItem> NonDefaultMenus(ToolStripItemCollection items)
-    {
-        foreach (ToolStripItem item in items)
-        {
-            if (item.Text != "Exit" &&
-                item.Text != "Options" &&
-                item.Text != "Open logs" &&
-                item.Text != "Raise issue")
-            {
-                yield return item;
-            }
-        }
-    }
+    static IEnumerable<ToolStripItem> NonDefaultMenus(ToolStripItemCollection items) =>
+        items
+            .Cast<ToolStripItem>()
+            .Where(_ => _.Text != "Exit" &&
+                        _.Text != "Options" &&
+                        _.Text != "Open logs" &&
+                        _.Text != "Raise issue" &&
+                        _.Text != "Purge verified files")
+            .ToList();
 
     static void RemovePreviousItems(ToolStripItemCollection items)
     {
         // Use ToList to avoid deferred execution of NonDefaultMenus
-        foreach (var item in NonDefaultMenus(items).ToList())
+        foreach (var item in NonDefaultMenus(items))
         {
             items.Remove(item);
         }
@@ -53,7 +50,7 @@ static class MenuBuilder
     static void DisposePreviousItems(ToolStripItemCollection items)
     {
         // Use ToList to avoid deferred execution of NonDefaultMenus
-        foreach (var item in NonDefaultMenus(items).ToList())
+        foreach (var item in NonDefaultMenus(items))
         {
             item.Dispose();
         }
@@ -66,11 +63,13 @@ static class MenuBuilder
             yield break;
         }
 
-        var deletes = tracker.Deletes
+        var deletes = tracker
+            .Deletes
             .OrderBy(_ => _.File)
             .ToList();
 
-        var moves = tracker.Moves
+        var moves = tracker
+            .Moves
             .OrderBy(_ => _.Temp)
             .ToList();
 
@@ -89,7 +88,8 @@ static class MenuBuilder
 
     static IEnumerable<ToolStripItem> BuildGroupedMenuItems(Tracker tracker, List<TrackedDelete> deletes, List<TrackedMove> moves)
     {
-        var groups = deletes.Select(_ => _.Group)
+        var groups = deletes
+            .Select(_ => _.Group)
             .Concat(moves.Select(_ => _.Group))
             .Distinct()
             .ToList();
@@ -100,8 +100,12 @@ static class MenuBuilder
             foreach (var toolStripItem in BuildMovesAndDeletes(
                          group,
                          tracker,
-                         deletes.Where(_ => _.Group == group).ToList(),
-                         moves.Where(_ => _.Group == group).ToList()))
+                         deletes
+                             .Where(_ => _.Group == group)
+                             .ToList(),
+                         moves
+                             .Where(_ => _.Group == group)
+                             .ToList()))
             {
                 yield return toolStripItem;
                 addedCount++;
@@ -147,8 +151,10 @@ static class MenuBuilder
 
     static ToolStripItem BuildDelete(TrackedDelete delete, Action accept)
     {
-        var menu = new ToolStripDropDownButton($"{delete.Name}");
-        menu.DropDownDirection = ToolStripDropDownDirection.Left;
+        var menu = new ToolStripDropDownButton($"{delete.Name}")
+        {
+            DropDownDirection = ToolStripDropDownDirection.Left
+        };
         menu.DropDownItems.Add(new MenuButton("Accept delete", accept));
         menu.DropDownItems.Add(BuildShowInExplorer(delete.File));
         return menu;
