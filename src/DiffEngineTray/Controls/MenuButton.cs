c15e31c82448b6b21df20824d64195425721d094
Simon Cropp <simon.cropp@gmail.com>
2024-07-18 16:23:00 +1000

    Update MenuButton.cs

--- src/DiffEngineTray/Controls/MenuButton.cs
+++ src/DiffEngineTray/Controls/MenuButton.cs
@@ -4,14 +4,16 @@ class MenuButton :
     public MenuButton(string text, Action? action = null, Image? image = null) :
         base(text, image)
     {
-        if (action != null)
+        if (action == null)
         {
-            Click += delegate
-            {
-                action();
-            };
-            CanSelect = true;
+            return;
         }
+
+        Click += delegate
+        {
+            action();
+        };
+        CanSelect = true;
     }
 
     public override bool CanSelect { get; }
