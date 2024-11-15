7f6200dc5d93542ec82f5f75f5a36fc92ba09c80
Simon Cropp <simon.cropp@gmail.com>
2024-11-15 22:30:52 +1100

    move System.Management back to 8.0.0

---
+++ src/DiffEngine.Tests/VersionTest.cs
@@ -0,0 +1,9 @@
+﻿public class VersionTest
+{
+    [Fact]
+    public void Run()
+    {
+        var version = typeof(System.Management.ManagementObject).Assembly.GetName().Version!;
+        Assert.Equal("8.0.0.0", version.ToString());
+    }
+}
