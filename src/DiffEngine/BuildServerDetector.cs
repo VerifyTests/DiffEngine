2e7f64b93eaa791973be59146e7aaa0a98cc4633
Simon Cropp <simon.cropp@gmail.com>
2025-03-06 16:39:27 +1100

    expose "type of CI" in BuildServerDetector

--- src/DiffEngine/BuildServerDetector.cs
+++ src/DiffEngine/BuildServerDetector.cs
@@ -6,83 +6,101 @@ public static class BuildServerDetector
 {
     static BuildServerDetector()
     {
-        // Appveyor
-        // https://www.appveyor.com/docs/environment-variables/
-        // Travis
-        // https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
-        if (string.Equals(Environment.GetEnvironmentVariable("CI"), "true", StringComparison.OrdinalIgnoreCase))
-        {
-            Detected = true;
-            return;
-        }
-
+        var variables = Environment.GetEnvironmentVariables();
         // Jenkins
         // https://wiki.jenkins.io/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-belowJenkinsSetEnvironmentVariables
-        if (Environment.GetEnvironmentVariable("JENKINS_URL") != null)
-        {
-            Detected = true;
-            return;
-        }
+        IsJenkins = variables.Contains("JENKINS_URL");
 
         // GitHub Action
         // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/using-environment-variables#default-environment-variables
-        if (Environment.GetEnvironmentVariable("GITHUB_ACTION") != null)
-        {
-            Detected = true;
-            return;
-        }
+        IsGithubAction = variables.Contains("GITHUB_ACTION");
+
+        // TeamCity
+        // https://www.jetbrains.com/help/teamcity/predefined-build-parameters.html#PredefinedBuildParameters-ServerBuildProperties
+        IsTeamCity = variables.Contains("TEAMCITY_VERSION");
+
+        // MyGet
+        // https://docs.myget.org/docs/reference/build-services#Available_Environment_Variables
+        IsMyGet = ValueEquals(variables, "BuildRunner", "MyGet");
+
+        // GitLab
+        // https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
+        IsGitLab = variables.Contains("GITLAB_CI");
+
+        // GoDC
+        // https://docs.gocd.org/current/faq/dev_use_current_revision_in_build.html
+        IsGoDc = variables.Contains("GO_SERVER_URL");
+
+        // Travis
+        // https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+        IsTravis = variables.Contains("TRAVIS_BUILD_ID");
+
+        // Docker
+        // https://www.hanselman.com/blog/detecting-that-a-net-core-app-is-running-in-a-docker-container-and-skippablefacts-in-xunit
+        IsDocker = ValueEquals(variables, "DOTNET_RUNNING_IN_CONTAINER", "true");
+
+        // AppVeyor
+        // https://www.appveyor.com/docs/environment-variables/
+        IsAppVeyor = variables.Contains("APPVEYOR");
 
         // AzureDevops
         // https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#agent-variables
         // Variable name is 'Agent.Id' to detect if this is a Azure Pipelines agent.
         // Note that variables are upper-cased and '.' is replaced with '_' on Azure Pipelines.
         // https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch#access-variables-through-the-environment
-        if (Environment.GetEnvironmentVariable("AGENT_ID") != null)
-        {
-            Detected = true;
-            return;
-        }
+        IsAzureDevops = !IsTravis &&
+                        !IsJenkins &&
+                        !IsGithubAction &&
+                        !IsTeamCity &&
+                        !IsGitLab &&
+                        !IsMyGet &&
+                        !IsGoDc &&
+                        !IsDocker &&
+                        !IsAppVeyor &&
+                        variables.Contains("AGENT_ID");
 
-        // TeamCity
-        // https://www.jetbrains.com/help/teamcity/predefined-build-parameters.html#PredefinedBuildParameters-ServerBuildProperties
-        if (Environment.GetEnvironmentVariable("TEAMCITY_VERSION") != null)
-        {
-            Detected = true;
-            return;
-        }
-
-        // MyGet
-        // https://docs.myget.org/docs/reference/build-services#Available_Environment_Variables
-        if (string.Equals(Environment.GetEnvironmentVariable("BuildRunner"), "MyGet", StringComparison.OrdinalIgnoreCase))
-        {
-            Detected = true;
-            return;
-        }
-
-        // GitLab
-        // https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
-        if (Environment.GetEnvironmentVariable("GITLAB_CI") != null)
-        {
-            Detected = true;
-            return;
-        }
-
-        // Docker
-        // https://www.hanselman.com/blog/detecting-that-a-net-core-app-is-running-in-a-docker-container-and-skippablefacts-in-xunit
-        if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
-        {
-            Detected = true;
-            return;
-        }
-
-        // GoDC
-        // https://docs.gocd.org/current/faq/dev_use_current_revision_in_build.html
-        if (Environment.GetEnvironmentVariable("GO_SERVER_URL") != null)
-        {
-            Detected = true;
-            return;
-        }
+        Detected = IsTravis ||
+                   IsJenkins ||
+                   IsGithubAction ||
+                   IsAzureDevops ||
+                   IsTeamCity ||
+                   IsGitLab ||
+                   IsMyGet ||
+                   IsGoDc ||
+                   IsDocker ||
+                   IsAppVeyor;
     }
+
+    static bool ValueEquals(IDictionary variables, string key, string value)
+    {
+        var variable = variables[key];
+        if(variable == null)
+        {
+            return false;
+        }
+
+        return string.Equals((string)variable, value, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public static bool IsAppVeyor { get; }
+
+    public static bool IsTravis { get; }
+
+    public static bool IsDocker { get; }
+
+    public static bool IsAzureDevops { get; }
+
+    public static bool IsGitLab { get; }
+
+    public static bool IsGoDc { get; }
+
+    public static bool IsMyGet { get; }
+
+    public static bool IsTeamCity { get; }
+
+    public static bool IsGithubAction { get; }
+
+    public static bool IsJenkins { get; }
 
     public static bool Detected { get; set; }
 }
