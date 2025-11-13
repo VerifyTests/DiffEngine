71866b796f3d58b7eaf9bd272c8ab4a4be0631ba
Simon Cropp <simon.cropp@gmail.com>
2025-11-13 15:22:24 +1100

    simplify usings

--- src/DiffEngineTray/GlobalUsings.cs
+++ src/DiffEngineTray/GlobalUsings.cs
@@ -1,2 +1,6 @@
 ﻿global using System.ComponentModel;
 global using Microsoft.Win32.SafeHandles;
+global using Serilog;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Diagnostics.CodeAnalysis;
